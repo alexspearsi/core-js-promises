b5ad1d6e579172302ce64d5b17ab3194e4b31173
Alex <strelchenko.aleksandr@yandex.ru>
2025-03-23 17:22:05 +0200

    feat: complete the 6th task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -110,8 +110,17 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const result = [];
+
+  promises.forEach((promise) => {
+    promise.then(
+      (res) => result.push(res),
+      () => result.push(null)
+    );
+  });
+
+  return Promise.resolve(result);
 }
 
 /**
